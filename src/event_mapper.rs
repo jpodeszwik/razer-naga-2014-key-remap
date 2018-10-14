17e71da26cdb7fad4b4031bdd7d451d2f1605ace
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-10-15 01:29:27 +0200

    Refactoring

--- src/event_mapper.rs
+++ src/event_mapper.rs
@@ -1,4 +1,5 @@
 use evdev_rs::enums::EventCode::{EV_KEY, EV_SYN};
+use evdev_rs::InputEvent;
 use super::keymap;
 use super::naga::Naga;
 use uinput::device::Device;
@@ -7,24 +8,26 @@ pub fn map_events(naga: Naga, input_device: &mut Device) {
     loop {
         let event = naga.next_event();
         match event {
-            Ok(k) => {
-                match k.1.event_code {
-                    EV_KEY(key) => {
-                        let key = keymap::map_key(key).unwrap();
-                        match k.1.value {
-                            1 => input_device.press(&key).unwrap(),
-                            0 => input_device.release(&key).unwrap(),
-                            _ => {}
-                        }
-                    }
-                    EV_SYN(_) => input_device.synchronize().unwrap(),
-                    _ => {}
-                }
-            }
+            Ok(k) => process_event(k.1, input_device),
             Err(e) => {
                 println!("Err: {}", e);
                 return;
             }
         }
+    }
+}
+
+fn process_event(event: InputEvent, input_device: &mut Device) {
+    match event.event_code {
+        EV_KEY(key) => {
+            let mapped_key = keymap::map_key(key).unwrap();
+            match event.value {
+                1 => input_device.press(&mapped_key).unwrap(),
+                0 => input_device.release(&mapped_key).unwrap(),
+                _ => {}
+            }
+        }
+        EV_SYN(_) => input_device.synchronize().unwrap(),
+        _ => {}
     }
 }
